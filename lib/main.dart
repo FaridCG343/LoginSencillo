9e084875c0d725f1e7e547e0a5253265bd385dd5
FlutLab.io <>
2021-05-19 17:43:04 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiLogin());
+
+class MiLogin extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AppMaster Castillo',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: IngresoSistema(),
+    ); //fin de material app
+  } //fin de widget build
+} //fin de clase MiLogin
+
+class IngresoSistema extends StatefulWidget {
+  @override
+  _IngresoSistemaState createState() => _IngresoSistemaState();
+} //fin clase ingresosistema
+
+class _IngresoSistemaState extends State<IngresoSistema> {
+  bool acceso = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+              icon: Icon(
+                Icons.tune,
+                semanticLabel: 'Filter',
+              ),
+              onPressed: () {
+                print('Filter button');
+              }),
+        ],
+        title: Text('Inicio de sesion Castillo'),
+      ), //fin de appbar
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/image3.jpg'),
+                SizedBox(height: 16.0),
+              ],
+            ),
+          ], //fin de widget[]
+        ), //fin de list view
+      ), //era segura
+    ); //fin de scaffold
+  }
+} //fin class _IngresoSistema
